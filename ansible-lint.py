92f3e1e65408a01dba07dc9c533993e64ebd5ce0
Kev <106404588+kevinjnicholson@users.noreply.github.com>
2022-08-04 20:14:40 +0100

    Update ansible-lint.py

--- ansible-lint.py
+++ ansible-lint.py
@@ -12,6 +12,3 @@ os.system("podman cp "+playbook+" 359ace75d4b3:/runner")
 os.system("podman exec 359ace75d4b3 ansible-lint /runner/"+playbook+"")
 os.system("podman exec 359ace75d4b3 rm /runner/"+playbook+"")
 os.system("podman stop 359ace75d4b3")
-
-if __name__ == "__main__":
-	main()
