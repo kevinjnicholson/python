f39f30793bb23bf7f87f62b7102d407b5233b5cc
Kev <106404588+kevinjnicholson@users.noreply.github.com>
2022-08-04 20:07:52 +0100

    Create ansible-lint.py

---
+++ ansible-lint.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#------------------------------------------------------ 
+# Created By: KJN
+# Created Date:  04/08/2022
+# version: 0.1
+# Title: Ansible Lint
+# -----------------------------------------------------
+import os
+playbook = input('Enter your playbook name: ')
+os.system("podman start 359ace75d4b3")
+os.system("podman cp "+playbook+" 359ace75d4b3:/runner")
+os.system("podman exec 359ace75d4b3 ansible-lint /runner/"+playbook+"")
+os.system("podman exec 359ace75d4b3 rm /runner/"+playbook+"")
+os.system("podman stop 359ace75d4b3")
+
+if __name__ == "__main__":
+	main()
